7a9b53a0ebf94ebecb783f589591fe737353bde2
Zander Whitehurst <zanderwhitehurst@gmail.com>
2015-06-10 10:24:01 +0100

    BIO Experience added

---
+++ src/components/bio/bio-ctrl.js
@@ -0,0 +1,65 @@
+(function () {
+    app.controller('BioCtrl', ['$scope', function ($scope) {
+
+        //var count = 0;
+        //
+        //var $nav = $('.btn');
+        //var $bioNav = $('.ed');
+        //var $bioUni = $('.each-uni');
+        //var $bioEx = $('.experience');
+        //
+        //$bioEx.click(function () {
+        //
+        //    if (++count % 2 == 0) {
+        //
+        //        $bioEx
+        //            .velocity('stop')
+        //            .velocity({top: 75}, 1000);
+        //        $bioNav
+        //            .velocity('stop')
+        //            .velocity("transition.slideUpOut", 1000);
+        //
+        //
+        //    }
+        //    return false;
+        //});
+        //
+        //
+        //$nav.click(function () {
+        //
+        //    if (++count % 2 == 1) {
+        //
+        //        $bioNav
+        //            .velocity('stop')
+        //            .velocity("transition.slideDownIn", 1000);
+        //        $bioEx
+        //            .velocity('stop')
+        //            .velocity("transition.slideDownIn", 2000);
+        //        $('.each-uni')
+        //            .velocity('stop')
+        //            .velocity("transition.slideUpOut", 500);
+        //
+        //    }
+        //    return false;
+        //});
+        //
+        //$bioNav.click(function () {
+        //
+        //    if (++count % 2 == 0) {
+        //
+        //        $bioEx
+        //            .velocity('stop')
+        //            .velocity("transition.slideUpOut", 500);
+        //        $('.each-uni')
+        //            .velocity('stop')
+        //            .velocity("transition.slideDownIn", 2000);
+        //
+        //
+        //    }
+        //    return false;
+        //});
+
+
+
+    }]);
+}());
